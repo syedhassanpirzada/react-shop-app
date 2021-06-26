8cdeda2242c4b22bd86b825e80325da03cbb02e6
syedhassanpirzada <hassanpirzada2121@gmail.com>
2021-06-26 22:38:29 +0500

    V 1.0 (For report)

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -37,7 +37,6 @@ function RightMenu(props) {
         <Menu.Item key="history">
           <a href="/history">History</a>
         </Menu.Item>
-
         <Menu.Item key="upload">
           <a href="/product/upload">Upload</a>
         </Menu.Item>
